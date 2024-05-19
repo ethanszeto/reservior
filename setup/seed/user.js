c3ff9e3b4ea81fa22a4810527fbe814e4d680d37
Ethan Szeto <ethan.szeto@gmail.com>
2024-05-19 01:19:37 -0400

    Added seeding data to users

---
+++ setup/seed/user.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    firstName: "Abbot",
+    lastName: "Wells",
+    username: "abbotwells",
+    password: "",
+    creationTime: new Date("2024-01-01"),
+    modificationTime: new Date("2024-01-01"),
+  },
+  {
+    firstName: "Lysander",
+    lastName: "Fitzroy",
+    username: "lysfitz",
+    password: "",
+    creationTime: new Date("2024-02-02"),
+    modificationTime: new Date("2024-02-02"),
+  },
+  {
+    firstName: "Reginald",
+    lastName: "Wyndham",
+    username: "regwind",
+    password: "",
+    creationTime: new Date("2024-03-03"),
+    modificationTime: new Date("2024-03-03"),
+  },
+];
