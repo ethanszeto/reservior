bc21cd29de8006bfeed48296e8335ae58aa54143
Ethan Szeto <ethan.szeto@gmail.com>
2024-05-19 23:17:22 -0400

    add tests to user

--- tests/__tests__/api_tests.js/user.js
+++ tests/__tests__/api_tests.js/user.js
@@ -3,8 +3,9 @@ import app from "../../../server/server.js";
 
 describe("User tests", () => {
   test("Test user sign up", async () => {
-    const response = await request(app).post("/user/create-account").send({});
+    const response = await request(app)
+      .post("/user/create-account")
+      .send({ firstName: "John", lastName: "Smith", username: "johns", password: "123" });
     expect(response.statusCode).toBe(200);
-    expect(JSON.stringify(response.body)).toBe(JSON.stringify({ message: "Connected to the reservior API!" }));
   });
 });
