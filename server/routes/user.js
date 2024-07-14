f33910ffa3908ee59557ce51c1e9a8196894a0fb
Ethan Szeto <ethan.szeto@gmail.com>
2024-07-13 23:00:59 -0400

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,10 @@ import bodyParser from "body-parser";
 import UserController from "../controllers/user_controller.js";
 import Authorize from "../auth/authorization.js";
 
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { ErrorInvalidToken, ErrorNotLoggedIn } from "../errors/http_error.js";
+
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }));
 
